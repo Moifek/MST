e1b017341c4b62a4519edbbbf6cea1da7c9ca99c
Moifek Maiza <79147993+Moifek@users.noreply.github.com>
2022-10-17 18:57:25 +0200

    input

---
+++ MST/MST/Program.cs
@@ -0,0 +1,54 @@
+﻿
+Console.WriteLine("Method ? : \n");
+int x = int.Parse(Console.ReadLine());
+if (x==1)
+{
+    Console.WriteLine("Nombre d'entrée(s) : \n");
+    int n = int.Parse(Console.ReadLine());
+    int[] Stock = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        Console.WriteLine($"{i + 1}\'ème élement:");
+        Stock[i] = int.Parse(Console.ReadLine());
+    }
+
+    Array.Reverse(Stock);
+
+    for (int i = 0; i < n; i++)
+    {
+        Console.WriteLine($"\n {Stock[i]}");
+    }
+
+}
+else
+{
+    Console.WriteLine("Nombre d'entrée(s) : \n");
+    int n = int.Parse(Console.ReadLine());
+    IDictionary<String,int> Stock = new Dictionary<String, int>();
+    for (int i = 0; i < n; i++)
+    {
+        Console.WriteLine($"{i + 1}\'ème élement [Format :: String ('XY')] :");
+
+        String name = Console.ReadLine();
+
+        Console.WriteLine($"{i + 1}\'ème élement {name}, [Format :: entier ('10') ] :");
+
+        int dist = int.Parse(Console.ReadLine());
+    }
+
+    Stock.OrderByDescending(x => x.Value);
+
+
+    foreach (KeyValuePair<String, int> item in Stock.OrderBy(x => x.Value))
+    {
+        Console.WriteLine("Key: {0}, Value: {1}", item.Key.ToString(), item.Value.ToString());
+    }
+
+
+}
+
+
+
+
+Console.WriteLine("traitement...");
+
--- mst.cs
+++
@@ -1,9 +0,0 @@
-using Internal;
-using System;
-class mst
-{
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("test");
-    }
-}
